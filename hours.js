7a5e35c8b4d31a7a733e868f8e1c05315b09ce7f
Martin Stone <martin@pagerduty.com>
2017-07-05 16:20:59 -0400

    summary

--- hours.js
+++ hours.js
@@ -30,7 +30,7 @@ function PDRequest(token, endpoint, method, options) {
 			} catch (e) {
 				alertStr += ".";
 			}
-			
+
 			try {
 				alertStr += "\n\n" + err.responseJSON.error.errors.join("\n");
 			} catch (e) {}
@@ -42,7 +42,6 @@ function PDRequest(token, endpoint, method, options) {
 
 	$.ajax(merged);
 }
-
 
 function fetchTeams(callback, offset, teams) {
 	var options = {
@@ -58,13 +57,12 @@ function fetchTeams(callback, offset, teams) {
 	if ( offset ) {
 		options['data'] = { offset: offset };
 	}
-	if ( ! teams ) { 
+	if ( ! teams ) {
 		teams = [];
 	}
 
 	PDRequest(getParameterByName('token'), "teams", "GET", options);
 }
-
 
 function fetchIncidents(since, until, callback, offset, incidents) {
 	var options = {
@@ -85,7 +83,7 @@ function fetchIncidents(since, until, callback, offset, incidents) {
 	if ( offset ) {
 		options.data.offset = offset;
 	}
-	
+
 	if ( $('#team-select').val() !== 'all' ) {
 		options.data['team_ids[]'] = $('#team-select').val();
 	}
@@ -97,24 +95,163 @@ function fetchIncidents(since, until, callback, offset, incidents) {
 	PDRequest(getParameterByName('token'), "incidents", "GET", options);
 }
 
+function fetchLogEntries(since, until, callback, offset, log_entries) {
+	var options = {
+		data: {
+			since: since.toISOString(),
+			until: until.toISOString(),
+			is_overview: true
+		},
+		success: function(data) {
+			Array.prototype.push.apply(log_entries, data.log_entries);
+			if ( data.more == true ) {
+				fetchLogEntries(since, until, callback, data.offset + data.limit, log_entries);
+			} else {
+				callback(log_entries);
+			}
+		}
+	}
+
+	if ( offset ) {
+		options.data.offset = offset;
+	}
+
+	if ( ! log_entries ) {
+		log_entries = [];
+	}
+
+	PDRequest(getParameterByName('token'), "log_entries", "GET", options);
+}
+
+function fetchLogEntriesParallel(since, until, callback) {
+	var limit = 100;
+	var infoFns = [];
+	var log_entries = [];
+
+	var options = {
+		data: {
+			since: since.toISOString(),
+			until: until.toISOString(),
+			is_overview: false,
+			total: true,
+			limit: limit
+		},
+		success: function(data) {
+			var total = data.total;
+			Array.prototype.push.apply(log_entries, data.log_entries);
+
+			if ( data.more == true ) {
+				var indexes = [];
+				for ( i = limit; i < total; i += limit ) {
+					indexes.push(Number(i));
+				}
+				indexes.forEach(function(i) {
+					var offset = i;
+					infoFns.push(function(callback) {
+						var options = {
+							data: {
+								since: since.toISOString(),
+								until: until.toISOString(),
+								is_overview: false,
+								total: true,
+								limit: limit,
+								offset: offset
+							},
+							success: function(data) {
+								Array.prototype.push.apply(log_entries, data.log_entries);
+								var progress = Math.round((log_entries.length / data.total) * 100);
+								$('#progressbar').attr("aria-valuenow", "" + progress);
+								$('#progressbar').attr("style", "width: " + progress + "%;");
+								$('#progressbar').html("" + progress + "%");
+								callback(null, data);
+							}
+						}
+						PDRequest(getParameterByName('token'), "log_entries", "GET", options);
+					});
+				});
+
+				async.parallel(infoFns, function(err, results) {
+					callback(log_entries);
+				});
+			} else {
+				callback(log_entries);
+			}
+		}
+	}
+	PDRequest(getParameterByName('token'), "log_entries", "GET", options);
+}
+
+function compareCreatedAt(a, b) {
+	return moment(a.created_at).diff(moment(b.created_at));
+}
+
+function secondsToHHMMSS(seconds) {
+	var hours = Math.floor(seconds / 60 / 60);
+	var minutes = Math.floor((seconds % 3600) / 60);
+	var seconds = seconds % 60;
+
+	var HH = ('0' + hours).slice(-2);
+	var MM = ('0' + minutes).slice(-2);
+	var SS = ('0' + seconds).slice(-2);
+
+	return `${HH}:${MM}:${SS}`;
+}
+
+function unique(array) {
+	return array.filter( function(value, index, self) {
+		return self.indexOf(value) === index;
+	});
+}
 
 function buildReport(since, until) {
 	$('.busy').show();
-	console.log(`Team selected: ${$('#team-select').val()}; Since: ${since}; Until: ${until}`);
 	async.series([
 		function(callback) {
-			fetchIncidents(since, until, function(fetchedData) {
-				incidents = fetchedData;
-				callback(null, fetchedData);
+			fetchLogEntriesParallel(since, until, function(data) {
+				$('.busy').hide();
+
+				incidents = {};
+				data.forEach(function(le) {
+					if ( ! incidents[le.incident.id] ) {
+						incidents[le.incident.id] = {}
+					}
+					if ( ! incidents[le.incident.id][le.type] ) {
+						incidents[le.incident.id][le.type] = [];
+					}
+
+					incidents[le.incident.id][le.type].push(le);
+				});
+
+				Object.keys(incidents).forEach(function(id) {
+					if ( ! incidents[id]['trigger_log_entry'] ) {
+						console.log(`incident ${id} has no trigger log entry. BALEETED.`);
+						delete incidents[id];
+					} else if ( ! incidents[id]['resolve_log_entry'] ) {
+						console.log(`incident ${id} has no resolve log entry. BALEETED.`);
+						delete incidents[id];
+					} else {
+						Object.keys(incidents[id]).forEach(function(leType) {
+							incidents[id][leType].sort(compareCreatedAt);
+						});
+
+						incidents[id].ttr = moment(incidents[id]['resolve_log_entry'][0].created_at).diff(moment(incidents[id]['trigger_log_entry'][0].created_at), 'seconds');
+
+						if ( incidents[id]['acknowledge_log_entry'] ) {
+							incidents[id].tta = moment(incidents[id]['acknowledge_log_entry'][0].created_at).diff(moment(incidents[id]['trigger_log_entry'][0].created_at), 'seconds');
+						}
+					}
+				});
+
+				callback(null, incidents);
 			});
 		},
 		function(callback) {
-			console.log(`fetched ${incidents.length} incidents`);
 			callback(null, 'yay');
 		}
 	],
 	function(err, results) {
 		var teamName = $('#team-select option:selected').text();
+		var teamID = $('#team-select option:selected').val();
 		var sinceStr = moment(since).format("LLLL");
 		var untilStr = moment(until).format("LLLL");
 
@@ -123,7 +260,7 @@ function buildReport(since, until) {
 
 		var workEndHHmm = $('#work-end-select').val().split(':');
 		var workEnd = moment().tz($('#tz-select').val()).hours(workEndHHmm[0]).minutes(workEndHHmm[1]);
-		
+
 		if( workEnd.isBefore(workStart) ) {
 			workEnd.add(1, 'days');
 		}
@@ -135,43 +272,73 @@ function buildReport(since, until) {
 			class: "display"
 		}));
 		$('#details-table').append('<thead><tr></tr></thead><tbody></tbody><tfoot><tr><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr></tfoot>');
-		
+
 		var tableData = [];
-		var resolvers = {};
-		console.log(`got ${incidents.length} incidents`);
-		incidents.forEach(function(incident) {
-			var created = moment.tz(incident.created_at, $('#tz-select').val());
-			var resolved = moment.tz(incident.last_status_change_at, $('#tz-select').val());
-			var duration = moment.duration(created.diff(resolved));
+		var assignees = {};
+		Object.keys(incidents).forEach(function(incidentID) {
+			var incident = incidents[incidentID];
+
+			var createdStr = incident.trigger_log_entry[0].created_at;
+			var created = moment.tz(createdStr, $('#tz-select').val());
+
+			var resolvedStr = incident.resolve_log_entry[0].created_at;
+			var resolved = moment.tz(resolvedStr, $('#tz-select').val());
+
+			var duration = moment.duration(resolved.diff(created));
+			var durationSecs = resolved.diff(created, 'seconds');
+
 			var createdTime = moment().tz($('#tz-select').val()).hours(created.hours()).minutes(created.minutes());
-			var resolvedBy = ( incident.last_status_change_by.type == 'user_reference' ) ? incident.last_status_change_by.summary : "auto-resolved";
+			var resolvedBy = ( incident.resolve_log_entry[0].agent.type == 'user_reference' ) ? incident.resolve_log_entry[0].agent.summary : "auto-resolved";
 
-			if ( ! resolvers[resolvedBy] ) {
-				resolvers[resolvedBy] = {
-					onHoursTime: 0,
-					offHoursTime: 0,
-					onHoursIncidents: 0,
-					offHoursIncidents: 0
-				}
-			}
+			var assignedTo = [];
 
-			if ( createdTime.isBetween(workStart, workEnd) ) {
-				resolvers[resolvedBy].onHoursTime += duration.seconds();
-				resolvers[resolvedBy].onHoursIncidents++;
+			if ( incident.assign_log_entry ) {
+				incident.assign_log_entry.forEach(function(le) {
+					le.assignees.forEach(function(assignee) {
+						if ( assignee.type == 'user_reference' ) {
+							assignedTo.push(assignee.summary);
+						}
+					});
+				});
+				assignedTo = unique(assignedTo);
+				assignedTo.sort();
+				assignedTo.forEach(function (assignee) {
+					if ( ! assignees[assignee] ) {
+						assignees[assignee] = {
+							onHoursTime: 0,
+							offHoursTime: 0,
+							onHoursIncidents: 0,
+							offHoursIncidents: 0
+						}
+					}
+					if ( createdTime.isBetween(workStart, workEnd) ) {
+						assignees[assignee].onHoursTime += durationSecs;
+						assignees[assignee].onHoursIncidents++;
+					} else {
+						assignees[assignee].offHoursTime += durationSecs;
+						assignees[assignee].offHoursIncidents++;
+					}
+				});
 			} else {
-				resolvers[resolvedBy].offHoursTime += duration.seconds();
-				resolvers[resolvedBy].offHoursIncidents++;
+				assignedTo = ['no one'];
 			}
+			var durationStr = duration.humanize();
+
+			var serviceName = incident.trigger_log_entry[0].service.summary;
+			var incidentSummary = incident.trigger_log_entry[0].incident.summary;
+			var incidentNumber = incidentSummary.match(/\[#([\d]+)\]/)[1];
+			var incidentURL = incident.trigger_log_entry[0].incident.html_url;
 
 			tableData.push([
-				'<a href="' + incident.html_url + '" target="blank">' + incident.incident_number + '</a>',
+				'<a href="' + incidentURL + '" target="blank">' + incidentNumber + '</a>',
 				created.format('l LTS [GMT]ZZ'),
 				createdTime.isBetween(workStart, workEnd) ? "no" : "yes",
+				assignedTo.join(', '),
 				resolved.format('l LTS [GMT]ZZ'),
 				resolvedBy,
 				duration.humanize(),
-				incident.service.summary,
-				incident.summary
+				serviceName,
+				incidentSummary
 			]);
 		});
 
@@ -180,6 +347,7 @@ function buildReport(since, until) {
 				{ title: "#" },
 				{ title: "Created at" },
 				{ title: "Off-Hours" },
+				{ title: "Assigned to" },
 				{ title: "Resolved at" },
 				{ title: "Resolved by" },
 				{ title: "Time to Resolve" },
@@ -194,7 +362,7 @@ function buildReport(since, until) {
 				'copy', 'csv', 'excel', 'pdf', 'print'
 			],
 			initComplete: function () {
-	            this.api().columns([2,4,6]).every( function () {
+	            this.api().columns([2,3,5,7]).every( function () {
 	                var column = this;
 	                var select = $('<label for="' + columnTitles[column.index()].title + '">' + columnTitles[column.index()].title + '</label><select id="' + columnTitles[column.index()].title + '"><option value=""></option></select>')
 	                    .appendTo( $(column.footer()).empty() )
@@ -202,20 +370,18 @@ function buildReport(since, until) {
 	                        var val = $.fn.dataTable.util.escapeRegex(
 	                            $(this).val()
 	                        );
-	 
+
 	                        column
 	                            .search( val ? '^'+val+'$' : '', true, false )
 	                            .draw();
 	                    } );
-	 
+
 	                column.data().unique().sort().each( function ( d, j ) {
-		                console.log(`select option ${d}`);
 	                    select.append( '<option value="'+d+'">'+d+'</option>' )
 	                } );
 	            } );
         	}
 		});
-
 
 		// build report table
 		$('#report').html('<h3>' + headline + '</h3>');
@@ -224,23 +390,34 @@ function buildReport(since, until) {
 			class: "display"
 		}));
 		var reportTableData = [];
-		Object.keys(resolvers).forEach(function(resolver) {
+
+		Object.keys(assignees).forEach(function(assignee) {
+			var onHoursMTTR = 'n/a';
+			if ( assignees[assignee].onHoursIncidents && assignees[assignee].onHoursTime ) {
+				onHoursMTTR = moment.duration(assignees[assignee].onHoursTime / assignees[assignee].onHoursIncidents, 'seconds').humanize()
+			}
+
+			var offHoursMTTR = 'n/a';
+			if ( assignees[assignee].offHoursIncidents && assignees[assignee].offHoursTime ) {
+				offHoursMTTR = moment.duration(assignees[assignee].offHoursTime / assignees[assignee].offHoursIncidents, 'seconds').humanize()
+			}
+
 			reportTableData.push([
-				resolver,
-				moment.duration(resolvers[resolver].onHoursTime, 'seconds').humanize(),
-				resolvers[resolver].onHoursIncidents,
-				moment.duration(resolvers[resolver].onHoursTime / resolvers[resolver].onHoursIncidents, 'seconds').humanize(),
-				moment.duration(resolvers[resolver].offHoursTime, 'seconds').humanize(),
-				resolvers[resolver].offHoursIncidents,
-				moment.duration(resolvers[resolver].onHoursTime / resolvers[resolver].onHoursIncidents, 'seconds').humanize(),
+				assignee,
+				secondsToHHMMSS(assignees[assignee].onHoursTime),
+				assignees[assignee].onHoursIncidents,
+				onHoursMTTR,
+				secondsToHHMMSS(assignees[assignee].offHoursTime),
+				assignees[assignee].offHoursIncidents,
+				offHoursMTTR,
 			]);
 		});
 		var reportColumnTitles = [
 				{ title: "User" },
-				{ title: "On-Hours Time" },
+				{ title: "On-Hours Time (HH:MM:SS)" },
 				{ title: "On-Hours Incidents" },
 				{ title: "On-Hours MTTR" },
-				{ title: "Off-Hours Time" },
+				{ title: "Off-Hours Time (HH:MM:SS)" },
 				{ title: "Off-Hours Incidents" },
 				{ title: "Off-Hours MTTR" },
 			];
@@ -253,8 +430,6 @@ function buildReport(since, until) {
 			]
 		});
 
-
-		console.log("all done");
 		$('.busy').hide();
 	});
 }
@@ -269,15 +444,15 @@ function main() {
 
 	var until = new Date();
 	var since = new Date();
-	since.setMonth(since.getMonth() - 1);
+	since.setDate(since.getDate() - 7);
 
 	since.setHours(0,0,0,0);
 	until.setHours(23,59,59,999);
 
 	$('#since').datepicker("setDate", since);
 	$('#until').datepicker("setDate", until);
-	
-	for ( h = 0; h < 24; h++ ) { 
+
+	for ( h = 0; h < 24; h++ ) {
 		for (m = 0; m < 60; m += 30 ) {
 			var timeStr = moment().hour(h).minute(m).format("HH:mm");
 			$('#work-start-select,#work-end-select').append($('<option/>', {
@@ -286,7 +461,7 @@ function main() {
 			}));
 		}
 	}
-	
+
 	if ( ! localStorage.getItem('workstart') ) {
 		localStorage.setItem('workstart', '08:00');
 	}
@@ -294,33 +469,32 @@ function main() {
 	if ( ! localStorage.getItem('workend') ) {
 		localStorage.setItem('workend', '20:00');
 	}
-	
+
 	$('#work-start-select').val(localStorage.getItem('workstart'));
 	$('#work-end-select').val(localStorage.getItem('workend'));
-	
+
 	moment.tz.names().forEach(function(tzName) {
 		$('#tz-select').append($('<option/>', {
 			value: tzName,
 			text: tzName
 		}));
 	});
-	
+
 	if ( ! localStorage.getItem('timezone') ) {
 		localStorage.setItem('timezone', moment.tz.guess() );
 	}
 
 	$('#tz-select').val(localStorage.getItem('timezone'));
-	
+
 	async.series([
 		function(callback) {
 			fetchTeams(function(teams) {
-				console.log(`Fetched ${teams.length} teams.`);
 				$('#team-select').html('');
 				$('#team-select').append($('<option/>', {
 					value: 'all',
 					text: 'All Teams'
 				}));
-		
+
 				teams.forEach(function(team) {
 					$('#team-select').append($('<option/>', {
 						value: team.id,
@@ -345,14 +519,14 @@ function main() {
 	$('#until').change(function() {
 		until = $('#until').datepicker("getDate");
 		until.setHours(23,59,59,999);
-		
+
 		buildReport(since, until);
 	});
 
 	$('#team-select').change(function() {
 		buildReport(since, until);
 	});
-	
+
 	$('#tz-select').change(function() {
 		localStorage.setItem('timezone', $('#tz-select').val());
 		buildReport(since, until);
@@ -382,8 +556,3 @@ function main() {
 }
 
 $(document).ready(main);
-
-
-
-
-
